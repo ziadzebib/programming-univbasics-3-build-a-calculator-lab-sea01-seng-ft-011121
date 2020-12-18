1620734be516ebc78f6995fd873f8ac05866a8ef
Ziad Zebib <zebibziad@gmail.com>
2020-12-18 01:35:14 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,25 @@
 def addition(num1, num2)
+  puts = (num1.to_i + num2.to_i)
 
 end
 
 def subtraction(num1, num2)
+  puts = (num1.to_i - num2.to_i)
 
 end
 
 def division(num1, num2)
-
+   puts = (num1.to_i / num2.to_i)
 end
 
 def multiplication(num1, num2)
-
+   puts = (num1.to_i * num2.to_i)
 end
 
 def modulo(num1, num2)
-
+   puts = (num1.to_i % num2.to_i)
 end
 
 def square_root(num)
-
+  puts = Math.sqrt(num.to_i)
 end
